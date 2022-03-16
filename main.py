0341ff3c40b309bbc8afc308b697c0504b1a8503
GiorgiaMusso <95913363+GiorgiaMusso@users.noreply.github.com>
2022-03-16 15:34:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+def f1_sum(a1,b1):
+  sum1 = a1 + b1
+  return sum1
+
+a1 = 1
+b1 = 2
+sum1 = 3
+
+f1_sum(100,100)
+
+print(sum1)
+
+def f2_sum(a2,b2):
+  sum2 = a2 + b2
+  return sum2
+
+a2 = 1 
+b2 = 2
+sum2 = 3
+
+print(f2_sum(100,100))
